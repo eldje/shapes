8bd07c63f54e35124fd6c1154adc6020a4d694f6
ulrik vdb <ulrik.vandenbulcke@gmail.com>
2016-05-08 17:35:29 +0200

    refactoring and added features

---
+++ A01 winform/A01 winform/Models/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace A01_winform.Models
+{
+    enum Color
+    {
+        Red,
+        Orange,
+        Green
+    };
+
+    abstract class Shape
+    {
+        public Shape(string name)
+        {
+            this.Name = name;
+            this.Area = 0;
+            this.Color = Color.Red;
+        }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public Color Color { get; set; }
+        
+        public override string ToString()
+        {
+            return Name;
+        }
+        protected void SetColor()
+        {
+            if (Area < 50)
+            {
+                Color = Color.Green;
+            }
+            else if (Area > 100)
+            {
+                Color = Color.Red;
+            }
+            else
+            {
+                Color = Color.Orange;
+            }
+        }
+    }
+}
---
+++ A01 winform/A01 winform/Repository/ShapeRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A01_winform.Models;
+
+namespace A01_winform.Repository
+{
+    enum ShapeForm
+    {
+        Circle,
+        Rectangle,
+        Square,
+        Triangle
+    }
+    class ShapeRepository
+    {
+        public List<Shape> Shapes { get; set; } = new List<Shape>();
+
+        public void AddShape(ShapeForm shapeForm, string name, params double[] args)
+        {
+            switch (shapeForm)
+            {
+                case ShapeForm.Circle:
+                    Shapes.Add(new Circle(name, args[0]));
+                    break;
+                case ShapeForm.Square:
+                    Shapes.Add(new Square(name, args[0]));
+                    break;
+                case ShapeForm.Triangle:
+                    Shapes.Add(new Triangle(name, args[0], args[1]));
+                    break;
+                case ShapeForm.Rectangle:
+                    Shapes.Add(new Rectangle(name,args[0], args[1]));
+                    break;
+            }
+        }
+    }
+}
